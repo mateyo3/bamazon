d5701941afbeb93bf27ce9c2593b159cfed558db
mateyo3 <mateoreyes720@gmail.com>
2017-11-09 18:08:39 -0800

    purchasePrompt function. Ask two questions

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -20,20 +20,41 @@ connection.connect(function(err) {
 
 function listProducts () {
 	var query = "SELECT * FROM products;"
-	  connection.query(query, function(err, res) {
-    for (var i = 0; i < res.length; i++) {
-      console.log(
-      	"Id: " + res[i].id + 
-      	"||" +
-      	"Product: " + res[i].product_name + 
-      	"||" +
-      	"Department: " + res[i].department_name + 
-      	"||" +
-      	"Price: " + res[i].price + 
-      	"||" +
-		"Stock Quantity: " + res[i].stock_quantity
-      	);
+		connection.query(query, function(err, res) {
+	for (var i = 0; i < res.length; i++) {
+		console.log(
+			"Id: " + res[i].id + 
+	      	"||" +
+	      	"Product: " + res[i].product_name + 
+	      	"||" +
+	      	"Department: " + res[i].department_name + 
+	      	"||" +
+	      	"Price: " + res[i].price + 
+	      	"||" +
+			"Stock Quantity: " + res[i].stock_quantity
+		);
     }
-  });
-}
+    purchasePrompts();
+  });//end: query function
+}//end: listProducts function
 
+function purchasePrompts() {
+	inquirer.prompt([
+		{
+			name: "id",
+			type: "input",
+			message: "What is the product ID that you want to purchase?"
+		},
+
+		{
+			name: "quantity",
+			type: "input",
+			message: "How many do you want to buy?"
+		},
+
+	]).then(function(user){
+
+		console.log("Product Id: " + user.id);
+		console.log("Quantity: " + user.quantity);
+	}); //end: function(user)
+} //end: purchasePrompt function
